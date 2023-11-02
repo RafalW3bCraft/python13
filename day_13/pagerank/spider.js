10573e38d19f318750bfe955f22bd6da5c88f139
Rafalc13 <seb.baadi@hotmail.com>
2023-11-02 09:11:22 +0530

    second commit init

---
+++ day_13/pagerank/spider.js
@@ -0,0 +1,12 @@
+spiderJson = {"nodes":[
+{"weight":1,"rank":0.0, "id":2, "url":"http://www.ribison.com"},
+{"weight":6,"rank":19.0, "id":3, "url":"http://www.ribison.com/index.html"},
+{"weight":6,"rank":19.0, "id":4, "url":"http://www.ribison.com/single-post.html"}],
+"links":[
+{"source":0,"target":0,"value":3},
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":1,"target":1,"value":3},
+{"source":1,"target":2,"value":3},
+{"source":2,"target":1,"value":3},
+{"source":2,"target":2,"value":3}]};
